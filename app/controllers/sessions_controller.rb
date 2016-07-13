1b99d413488bbd9e85877d36a7fec5b973432923
tungtram <tibuchivn@gmail.com>
2016-07-13 20:49:54 +0700

    milestone2

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,20 @@
 class SessionsController < ApplicationController
   def create
-    redirect_to users_path
+    @email = params[:email]
+    @pass = params[:password]
+
+    @user = User.find_by(email: @email)
+    if @user
+      if @user.authenticate(@pass)
+        session[:user_id] = @user.id
+        return redirect_to users_path
+      end
+    end
+    redirect_to login_path
+  end
+
+  def destroy
+    session[:user_id] = nil
+    redirect_to root_path
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   root 'welcome#index'
   resources :users
-  get 'login' => "sessions#create"
+  get 'login' => "users#login"
+  get 'logout' => "sessions#destroy"
+  post "sessions/create"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
